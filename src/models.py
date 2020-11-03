8baf00a27ea553c549c6cfa67faf74790b1921dc
nayash <lotus.nayak@gmail.com>
2020-11-04 00:42:38 +0530

    data saving code done, models imported

---
+++ src/models.py
@@ -0,0 +1,220 @@
+from torch import nn
+import torch.functional as F
+from torch.nn import init
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, word_emb_tensor,batch_first=True,
+                 layers=1, bidirectional=False, dropout=0.0):
+        super(Encoder, self).__init__()
+
+        self.batch_first = batch_first
+        self.layers = layers
+        self.hidden_size = hidden_size
+        self.bidirectional = bidirectional
+        self.input_size = input_size  # size of input vocab
+        self.emb = nn.Embedding(input_size, hidden_size)
+        self.emb.load_state_dict({'weight': word_emb_tensor})
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=batch_first,
+                            num_layers=layers, bidirectional=bidirectional,
+                            dropout=dropout)  # input_size = len of sequence
+
+        # self.apply(self.weights_init)
+
+    def forward(self, input, hidden):
+        emb = self.emb(input)  # (batch, input_size, hidden_size)
+        if not self.batch_first:
+            emb = emb.permute(1, 0, 2)
+
+        # (o,h) => (torch.Size([128, 20, 200]),torch.Size([2, 128, 100]),
+        # torch.Size([2, 128, 100]))
+        o, h = self.lstm(emb, hidden)
+        return o, h
+
+    def initHidden(self, batch_size):
+        return (torch.zeros(self.layers*(1 if not self.bidirectional else 2),
+                            batch_size, self.hidden_size, device=device,
+                            requires_grad=True),
+                torch.zeros(self.layers*(1 if not self.bidirectional else 2),
+                            batch_size, self.hidden_size, device=device,
+                            requires_grad=True))
+
+    def weights_init(self, m):
+        if isinstance(m, torch.nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant(param, 0.0)
+                elif 'weight' in name:
+                    nn.init.xavier_normal(param)
+        else:
+            init.xavier_uniform_(m.weight.data)
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_size, hidden_size, word_emb_tensor, batch_first=True,
+                 layers=1, bidirectional=False, dropout=0.0):
+        super(Decoder, self).__init__()
+
+        self.batch_first = batch_first
+        self.output_size = output_size  # output vocab
+        self.bidirectional = bidirectional
+        self.layers = layers
+        self.hidden_size = hidden_size
+        self.emb = nn.Embedding(output_size, hidden_size)
+        self.emb.load_state_dict({'weight': word_emb_tensor})
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=batch_first,
+                            num_layers=layers, bidirectional=bidirectional,
+                            dropout=dropout)  # input_size = len of sequence
+        self.linear = nn.Linear(
+            (1 if not self.bidirectional else 2)*hidden_size, output_size)
+        self.lrelu = nn.LeakyReLU(0.2)
+        self.log_softmax = nn.LogSoftmax(dim=2)
+
+    def forward(self, input, hidden):
+        emb = self.emb(input)  # (batch, seq_len, hidden_size)
+        if not self.batch_first:
+            emb = emb.permute(1, 0, 2)
+        o, h = self.lstm(emb, hidden)  # output/hidden => (1, 1, 256 or 512)
+        x = self.log_softmax(self.linear(o))
+        return x, h
+
+    def initHidden(self, batch_size):
+        return (torch.zeros(self.layers*(1 if not self.bidirectional else 2),
+                            batch_size, self.hidden_size, device=device,
+                            requires_grad=True),
+                torch.zeros(self.layers*(1 if not self.bidirectional else 2),
+                            batch_size, self.hidden_size, device=device,
+                            requires_grad=True))
+
+    def weights_init(self, m):
+        if isinstance(m, torch.nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant(param, 0.0)
+                elif 'weight' in name:
+                    nn.init.xavier_normal(param)
+        else:
+            init.xavier_uniform_(m.weight.data)
+
+
+class GeneratorModel(nn.Module):
+    def __init__(self, input_vocab, hidden_size, batch_size, word_emb_tensor,
+                 batch_first=True, layers=1, bidirectional=False, lstm_do=0.0):
+        super(GeneratorModel, self).__init__()
+        self.batch_size = batch_size
+        self.batch_first = batch_first
+        self.encoder = Encoder(input_vocab, hidden_size, word_emb_tensor, batch_first=batch_first,
+                               bidirectional=bidirectional, layers=layers,
+                               dropout=lstm_do)
+        self.decoder = Decoder(input_vocab, hidden_size, word_emb_tensor, layers=layers,
+                               bidirectional=bidirectional,
+                               batch_first=batch_first, dropout=lstm_do)
+        self.enc_sos = None
+        self.dec_sos = None
+        # self.linear_topk = nn.Linear()
+
+    def forward(self, x):
+        enc_out, h = self.encoder(x, self.encoder.initHidden(
+            x.size(self.get_batch_index(self.batch_first))))
+        decoder_out = []
+        decoder_raw = []
+
+        decoder_input = torch.tensor([[self.dec_sos]]*x.size(
+            self.get_batch_index(self.batch_first)), device=device)
+
+        decoder_hidden = h
+
+        for i in range(max_len):
+            out, decoder_hidden = self.decoder(
+                decoder_input, decoder_hidden)  # out=> [1, 1, vocab]
+            pred, idx = out.topk(1)
+            decoder_input = idx.view(idx.size(0), 1)
+            decoder_out.append(idx.squeeze())
+            decoder_raw.append(out)
+
+        if len(decoder_out[0].size()) > 0:
+            stack_dim = 1
+        else:
+            stack_dim = 0
+        out1 = torch.stack(decoder_out, dim=stack_dim)
+        out2 = torch.stack(decoder_raw, dim=stack_dim)
+        return out1, out2.squeeze(), enc_out
+
+    def get_batch_index(self, batch_first):
+        return 0 if batch_first else 1
+
+    def set_mode(self, mode):
+        if mode == src2src:
+            self.enc_sos = word2idx[SOS_SRC]
+            self.dec_sos = word2idx[SOS_SRC]
+        elif mode == tgt2tgt:
+            self.enc_sos = word2idx[SOS_TGT]
+            self.dec_sos = word2idx[SOS_TGT]
+        elif mode == src2tgt:
+            self.enc_sos = word2idx[SOS_SRC]
+            self.dec_sos = word2idx[SOS_TGT]
+        elif mode == tgt2src:
+            self.enc_sos = word2idx[SOS_TGT]
+            self.dec_sos = word2idx[SOS_SRC]
+
+
+# class LatentClassifier(nn.Module):
+#     def __init__(self, input_shape, output_shape, hidden_nodes):
+#         super(LatentClassifier, self).__init__()
+#         self.input_shape = input_shape
+#         self.output_shape = output_shape
+#         self.hidden_nodes = hidden_nodes
+
+#         self.linear1 = nn.Linear(input_shape, hidden_nodes)
+#         self.linear2 = nn.Linear(hidden_nodes, int(hidden_nodes/4))
+#         self.linear3 = nn.Linear(int(hidden_nodes/4), output_shape)
+#         self.lrelu = nn.LeakyReLU(0.2)
+#         self.sigmoid = nn.Sigmoid()
+#         self.softmax = nn.Softmax(dim=1)
+
+#     def forward(self, input):
+#         x = self.linear1(input)  # [bs, 21, hidden_nodes]
+#         # print('clf1', x.shape)
+#         x = self.lrelu(self.linear2(x))  # [bs, 21, hidden_nodes/4]
+#         # print('clf2', x.shape)
+#         x = self.sigmoid(self.linear3(x))  # [bs, 21, 1]
+#         # print('clf3', x.shape)
+#         x = x.squeeze()
+#         x = x.prod(dim=1)
+#         # print('clf4', x.shape)
+#         x = self.sigmoid(x)
+#         # print('clf5', x.shape)
+#         return x.squeeze()  # [batch_size]
+
+
+class LatentClassifier(nn.Module):
+    def __init__(self, input_shape, output_shape, hidden_nodes):
+        super(LatentClassifier, self).__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.hidden_nodes = hidden_nodes
+
+        self.linear1 = nn.Linear(input_shape, hidden_nodes)
+        self.linear2 = nn.Linear(hidden_nodes, int(hidden_nodes/2))
+        self.linear3 = nn.Linear(int(hidden_nodes/2), int(hidden_nodes/8))
+        # self.linear4 = nn.Linear(int(hidden_nodes/4), int(hidden_nodes/8))
+        self.linear5 = nn.Linear(int(hidden_nodes/8), int(hidden_nodes/32))
+        # self.linear6 = nn.Linear(int(hidden_nodes/16), int(hidden_nodes/32))
+        self.linear7 = nn.Linear(int(hidden_nodes/32), output_shape)
+        self.bn3 = nn.BatchNorm1d(num_features=int(hidden_nodes/8))
+        self.bn5 = nn.BatchNorm1d(num_features=int(hidden_nodes/32))
+        self.lrelu = nn.LeakyReLU(0.2)
+
+    def forward(self, input):
+        x = self.lrelu(self.linear1(input))
+        x = self.lrelu(self.linear2(x))
+        x = self.lrelu(self.bn3(self.linear3(x)))
+        # x = self.lrelu(self.linear4(x))
+        x = self.lrelu(self.bn5(self.linear5(x)))
+        # x = self.lrelu(self.linear6(x))
+        # x = self.sigmoid(self.linear7(x))
+        x = self.linear7(x)
+        return x.squeeze()
+
+    def weights_init(self, m):
+        init.xavier_uniform_(m.weight.data)
--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+from models import *
 import os
 import pickle
 from pathlib import Path
@@ -14,7 +15,10 @@ from tqdm.auto import tqdm
 from logger import Logger
 from utils import *
 from collections import Counter
-
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import RandomSampler, random_split
+import torch.nn.init as init
 
 '''
 supported arguments
@@ -34,28 +38,27 @@ all supported keys.
 
 '''
 
-
 seed = 999
 np.random.seed(seed)
 
 ROOT_PATH = Path(os.path.dirname(os.path.realpath(__file__))).parent
-INPUT_PATH = ROOT_PATH/'inputs'
-OUTPUT_PATH = ROOT_PATH/'outputs'
-GLOVE_PATH = INPUT_PATH/'glove.6B.200d.txt'
-YELP_PATH = INPUT_PATH/'yelp-reviews-preprocessed'
+INPUT_PATH = ROOT_PATH / 'inputs'
+OUTPUT_PATH = ROOT_PATH / 'outputs'
+GLOVE_PATH = INPUT_PATH / 'glove.6B.200d.txt'
+YELP_PATH = INPUT_PATH / 'yelp-reviews-preprocessed'
 src_sents = 'sentiment.0.train.txt'
 tgt_sents = 'sentiment.1.train.txt'
 
 arg_parser = argparse.ArgumentParser()
 
-arg_parser.add_argument('-s', '--insrc', default=YELP_PATH/src_sents,
+arg_parser.add_argument('-s', '--insrc', default=YELP_PATH / src_sents,
                         help='path for source data file with each source \
                         sentence in a new line. Default is yelp negative')
 
-arg_parser.add_argument('-t', '--intgt', default=YELP_PATH/tgt_sents,
+arg_parser.add_argument('-t', '--intgt', default=YELP_PATH / tgt_sents,
                         help='path for target data file with each target \
                         sentence in a new line. Default is yelp dataset')
-arg_parser.add_argument('-c', '--config', default=INPUT_PATH/'config.json',
+arg_parser.add_argument('-c', '--config', default=INPUT_PATH / 'config.json',
                         help='configuration/hyperparameters in json format')
 arg_parser.add_argument('-e', '--expid', default='temp',
                         help='identifier for track your experiment. This is\
@@ -63,11 +66,10 @@ arg_parser.add_argument('-e', '--expid', default='temp',
                         to this experiment will be stored in a specific \
                         folder. If passed run_id already exists, exception \
                         is be thrown. Use special "temp" for test runs.')
-arg_parser.add_argument('-f', '--force', default=False,
+arg_parser.add_argument('-f', '--force', default=False, action='store_true',
                         help='if True then the data clean up processing \
                         is done again instead of of using the saved data \
                         checkpoints')
-
 
 args = arg_parser.parse_args()
 source_file_path = os.path.abspath(args.insrc)
@@ -76,9 +78,19 @@ config_path = os.path.abspath(args.config)
 run_id = args.expid
 force_preproc = args.force
 
-run_path = OUTPUT_PATH/'runs'/run_id
-log_path = run_path/'logs'
-data_cp_path = OUTPUT_PATH/'data_cp.pk'
+run_path = OUTPUT_PATH / 'runs' / run_id
+log_path = run_path / 'logs'
+data_cp_path = OUTPUT_PATH / 'data_cp.pk'
+tensors_path = OUTPUT_PATH / 'data_tensors_cp.pt'
+
+src2tgt = 'C2NQ'
+tgt2src = 'NQ2C'
+src2src = 'C2C'
+tgt2tgt = 'NQ2NQ'
+
+SOS_SRC = 'SOSSRC'
+SOS_TGT = 'SOSTGT'
+
 
 with open(source_file_path, encoding='utf-8',
           errors='ignore') as file:
@@ -94,12 +106,11 @@ else:
     if run_id != 'temp':
         raise Exception('run_id already exists')
 
-
 logger = Logger(str(log_path), run_id, std_out=True)
 with open(config_path, 'r') as file:
     _ = file.read()
     config_dict = json.loads(_)
-print(config_dict)
+logger.append_log('config: ', config_dict)
 
 if force_preproc or not data_cp_path.exists():
     logger.append_log('cleaning up sentences...')
@@ -114,39 +125,39 @@ if force_preproc or not data_cp_path.exists():
     words = list(set(words))
     logger.append_log('number of words', len(words))
 
+    logger.append_log('fetching word embeddings from Glove ...')
+    extra_tokens = [SOS_SRC, SOS_TGT, 'EOS', 'PAD', 'UNK']
+    words.extend(extra_tokens)
+    word2idx, idx2word, word_emb, diff = vocab_from_pretrained_emb(
+        GLOVE_PATH, words)
+
     data_cp = {'tgt': tgt_sents, 'src': src_sents, 'words': words}
-    max_len = max([len(word_tokenize(sent)) for sent in src_sents+tgt_sents])
+    max_len = max([len(word_tokenize(sent)) for sent in src_sents + tgt_sents])
     data_cp['max_sent_len'] = max_len
+    data_cp['word2idx'] = word2idx
+    data_cp['idx2word'] = idx2word
+    data_cp['word_emb'] = word_emb
     pickle.dump(data_cp, open(str(data_cp_path), 'wb'))
     logger.append_log('saved cleaned up sentences at {}, file size {}\
-     KB'.format(str(data_cp_path), data_cp_path.stat().st_size//1024))
-else:
-    data_cp = pickle.load(open(str(data_cp_path), 'rb'))
-    tgt_sents = data_cp['tgt']
-    src_sents = data_cp['src']
-    words = data_cp['words']
-    max_len = data_cp['max_len']
-    max_len += 3  # extra tokens for SOS, EOS, OpType
-    print(len(src_sents), len(tgt_sents), len(my_words), max_len)
+            KB'.format(str(data_cp_path), data_cp_path.stat().st_size // 1024))
 
-logger.append_log('number of samples in yelp negative and positive are\
+data_cp = pickle.load(open(str(data_cp_path), 'rb'))
+tgt_sents = data_cp['tgt']
+src_sents = data_cp['src']
+words = data_cp['words']
+max_len = data_cp['max_sent_len']
+max_len += 3  # extra tokens for SOS, EOS, OpType
+word2idx = data_cp['word2idx']
+idx2word = data_cp['idx2word']
+word_emb = data_cp['word_emb']
+print(len(src_sents), len(tgt_sents), len(words), max_len)
+
+logger.append_log('number of samples in source and target are\
  {}, {}'.format(len(src_sents), len(tgt_sents)))
 
-
-src2tgt = 'C2NQ'
-tgt2src = 'NQ2C'
-src2src = 'C2C'
-tgt2tgt = 'NQ2NQ'
-
-SOS_SRC = 'SOSSRC'
-SOS_TGT = 'SOSTGT'
-
-# 'SOS', src2tgt, tgt2src, src2src, tgt2tgt
-extra_tokens = [SOS_SRC, SOS_TGT, 'EOS', 'PAD', 'UNK']
-my_words.extend(extra_tokens)
-word2idx, idx2word, word_emb, diff = vocab_from_pretrained_emb(
-    GLOVE_PATH, my_words)
 word_emb_tensor = torch.tensor(word_emb)
+for k, v in word2idx.items():
+    assert k == idx2word[v]
 
 
 def sent_to_tensor(sentence, word2idx=word2idx, max_len=max_len,
@@ -164,14 +175,14 @@ def sent_to_tensor(sentence, word2idx=word2idx, max_len=max_len,
         # don't drop NER mask token
         if not words[drop_idx].isupper() and \
                 not words[drop_idx] == '.' and not words[drop_idx] == '?':
-            words = words[:drop_idx]+words[drop_idx+1:]
+            words = words[:drop_idx] + words[drop_idx + 1:]
 
     if shuffle:
         words = permute_items(words, k=4)
 
     temp.extend([word2idx.get(w, word2idx['UNK']) for w in words])
     temp.append(word2idx['EOS'])
-    temp.extend([word2idx['PAD']] * (max_len-len(temp)))
+    temp.extend([word2idx['PAD']] * (max_len - len(temp)))
     return torch.tensor(temp)  # , device=device
 
 
@@ -187,7 +198,7 @@ def get_noisy_tensor(tensor, drop_prob=0.1, k=3, word2idx=word2idx):
     try:
         eos_index = (tensor == word2idx['EOS']).nonzero(as_tuple=False)[0][0]
     except:
-        eos_index = tensor.size(0)-1
+        eos_index = tensor.size(0) - 1
     proper_tensor = tensor[1:eos_index]
     final = torch.zeros(tensor.size())
     final[:] = word2idx['PAD']
@@ -197,8 +208,8 @@ def get_noisy_tensor(tensor, drop_prob=0.1, k=3, word2idx=word2idx):
     res = proper_tensor[drop_mask > drop_prob]
 
     res = permute_tensor(res)
-    final[1:res.size(0)+1] = res
-    final[res.size(0)+1] = word2idx['EOS']
+    final[1:res.size(0) + 1] = res
+    final[res.size(0) + 1] = word2idx['EOS']
     return final.long()
 
 
@@ -208,9 +219,9 @@ def get_noisy_tensor_grad(tensor, drop_prob=0.1, k=3, word2idx=word2idx):
             eos_index = (tensor == word2idx['EOS']).nonzero(
                 as_tuple=False)[0][0]
         except:
-            eos_index = tensor.size(0)-1
+            eos_index = tensor.size(0) - 1
 
-        tensor[1:eos_index] = tensor[1:eos_index][torch.randperm(eos_index-1)]
+        tensor[1:eos_index] = tensor[1:eos_index][torch.randperm(eos_index - 1)]
         # drop_mask = torch.FloatTensor(tensor[1:eos_index].size(0)).uniform_(0, 1)
     except Exception as e:
         print('Exception', e, word2idx['EOS'], eos_index)
@@ -223,5 +234,132 @@ def row_apply(tensor, func, is_stack=True):
         res.append(func(row))
     return torch.stack(res) if is_stack else res
 
+
+def weights_init(m):
+    try:
+        if isinstance(m, torch.nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant_(param, 0.0)
+                    # print('filled', m)
+                elif 'weight' in name:
+                    nn.init.xavier_normal_(param)
+                    # print('filled', m)
+        else:
+            if not isinstance(m, nn.Embedding):
+                init.xavier_normal_(m.weight.data)
+                # print('filled', m)
+    except:
+        # print('failed', m)
+        pass
+
+
+def isNan(tensor):
+    return torch.isnan(tensor).any().item() or torch.isinf(tensor).any().item()
+
+
+if force_preproc or not tensors_path.exists():
+    logger.append_log('converting sentences to tensors...')
+    x_src = []
+    y_src = []
+    x_tgt = []
+    y_tgt = []
+
+    natq_label = 1
+    cloze_label = 0
+
+    permute_prob = config_dict['permute_prob']
+    drop_noise = config_dict['drop_noise']
+
+    for i, cloze in enumerate(src_sents):
+        tensor = sent_to_tensor(cloze.strip(), word2idx, max_len, type='neg')
+        x_src.append(get_noisy_tensor(tensor))
+        y_src.append(tensor.clone())
+
+    print('len x_src', len(x_src))
+
+    for i, q in enumerate(tgt_sents):
+        tensor = sent_to_tensor(q.strip(), word2idx, max_len, type='pos')
+        x_tgt.append(get_noisy_tensor(tensor))
+        y_tgt.append(tensor.clone())
+
+    print('len x_tgt', len(x_tgt))
+    x_src = torch.stack(x_src)
+    y_src = torch.stack(y_src)
+    x_tgt = torch.stack(x_tgt)
+    y_tgt = torch.stack(y_tgt)
+
+    delta = x_tgt.shape[0] - x_src.shape[0]
+    if delta > 0:
+        indexes = np.random.choice(x_src.size(0), delta)
+        x_src = torch.cat((x_src, x_src[indexes]))
+        y_src = torch.cat((y_src, y_src[indexes]))
+    else:
+        indexes = np.random.choice(x_tgt.size(0), np.abs(delta))
+        x_tgt = torch.cat((x_tgt, x_tgt[indexes]))
+        y_tgt = torch.cat((y_tgt, y_tgt[indexes]))
+
+    assert len(x_src) == len(x_tgt)
+    # x_src.half(), y_src.half(), x_tgt.half(), y_tgt.half()
+
+    data_tensors_cp = {'x_src': x_src, 'y_src': y_src,
+                       'x_tgt': x_tgt, 'y_tgt': y_tgt}
+    torch.save(data_tensors_cp, str(tensors_path))
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+if device == 'cpu':
+    logger.append_log('warning!!! CPU device is being used')
+data_tensors_cp = torch.load(tensors_path)
+
+x_src = data_tensors_cp['x_src']
+y_src = data_tensors_cp['y_src']
+x_tgt = data_tensors_cp['x_tgt']
+y_tgt = data_tensors_cp['y_tgt']
+
+x_src = x_src.to(device)
+y_src = y_src.to(device)
+x_tgt = x_tgt.to(device)
+y_tgt = y_tgt.to(device)
+logger.append_log('loaded tensors...', x_src.shape, y_src.shape,
+                  x_tgt.shape, y_tgt.shape, x_src.is_cuda)
+
+# create data loaders
+total_samples = len(x_src)
+train_size = int(0.95 * total_samples)
+test_size = total_samples - train_size
+ds_cloze = TensorDataset(x_src, y_src)
+ds_natq = TensorDataset(x_tgt, y_tgt)
+ds_cloze_train, ds_cloze_test = random_split(ds_cloze, [train_size, test_size],
+                                             generator=torch.Generator().manual_seed(seed))
+ds_natq_train, ds_natq_test = random_split(ds_natq, [train_size, test_size],
+                                           generator=torch.Generator().manual_seed(seed))
+
+dl_cloze_train = DataLoader(ds_cloze_train, batch_size=config_dict['batch_size'], drop_last=True, num_workers=0,
+                            pin_memory=False)
+dl_natq_train = DataLoader(ds_natq_train, batch_size=config_dict['batch_size'], drop_last=True, num_workers=0,
+                           pin_memory=False)
+
+dl_cloze_test = DataLoader(ds_cloze_test, batch_size=1)
+dl_natq_test = DataLoader(ds_natq_test, batch_size=1)
+
+logger.append_log('train/test size', len(dl_cloze_train), len(dl_cloze_test))
+
+input_vocab = len(word2idx)
+
+generator = GeneratorModel(input_vocab, config_dict['hidden_dim'],
+                           config_dict['batch_size'], word_emb_tensor, layers=config_dict['layers'],
+                           bidirectional=config_dict['bidir'], lstm_do=config_dict['lstm_do'])
+
+# lat_clf = LatentClassifier((2 if config_dict['bidir'] else 1) * config_dict['hidden_dim'], 
+#                            1, config_dict['clf_hidden_nodes'])
+
+clf_in_shape = max_len * (2 if config_dict['bidir'] else 1) * config_dict['hidden_dim']
+lat_clf = LatentClassifier(clf_in_shape, 1, int(clf_in_shape / 1.5))
+# lat_clf = LatentClassifier((2 if config_dict['bidir'] else 1)*config_dict['hidden_dim'], 1, 
+#                            2*(2 if config_dict['bidir'] else 1)*config_dict['hidden_dim'])
+assert not isNan(generator.encoder.emb.weight)
+# generator.half(), lat_clf.half()
+generator.to(device), lat_clf.to(device)
+generator.apply(weights_init), lat_clf.apply(weights_init)
 
 logger.flush()
