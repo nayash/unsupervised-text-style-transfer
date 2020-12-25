df75e4a47b76d5da0e9258476206f88f2183f88d
Asutosh <lotus.nayak@gmail.com>
2020-12-25 08:38:22 +0530

    eval.py bug fix

--- src/eval.py
+++ src/eval.py
@@ -18,7 +18,8 @@ python eval.py --expid torchAttn_2lstm_smallData -f ../inputs/test_sentences.txt
 python eval.py --expid 1dirDec_300emb_largData_max8 -f ../inputs/test_sentences.txt --cpfile data_cp8.pk
 python eval.py --expid fr_en-learnableEmbDim-pt1DO -f ../inputs/test_sentences_fr.txt --cpfile data_cp15.pk
 python eval.py --expid st-yelp_freezeEmb -f ../inputs/test_sentences.txt --cpfile ../outputs/runs/st-yelp_freezeEmb/data_cp10.pk --model best_modelG_2.pt
-python eval.py --expid st-yelp_freezeEmb-attn -f ../inputs/test_sentences.txt --cpfile ../outputs/runs/st-yelp_freezeEmb/data_cp10.pk --model best_modelG_1.pt --config '../inputs/config.json' 
+python eval.py --expid st-yelp_freezeEmb-attn -f ../inputs/test_sentences.txt --cpfile ../outputs/runs/st-yelp_freezeEmb/data_cp10.pk --model best_modelG_1.pt --config '../inputs/config.json'
+python eval.py --expid st-yelp_frzEmb-hidDimDiff -f ../inputs/test_sentences.txt --model best_modelG_14.pt --config '../inputs/config.json' --cpfile '../outputs/runs/st-yelp_frzEmb-hidDimDiff/data_cp10.pk'
 
 /home/asutosh/Documents/ml_projects/unsupervised-text-style-transfer/outputs/runs/1dirDec_300emb_largData_max8
 '''
@@ -33,21 +34,24 @@ tgt_sents = 'sentiment.1.all.txt'
 
 arg_parser = argparse.ArgumentParser()
 
-arg_parser.add_argument('--expid', help='experiment id for the model to evaluate')
-arg_parser.add_argument('-f', help='text file with sentences to evaluate the model'
-                                   ' with')
+arg_parser.add_argument('--expid',
+                        help='experiment id for the model to evaluate')
+arg_parser.add_argument('-f',
+                        help='text file with sentences to evaluate the model'
+                             ' with')
 arg_parser.add_argument('--cleanfunc', default='clean_text_yelp',
                         help='text cleaning function. use the same function '
                              'used for treating training data. for e.g. see '
                              '"clean_text_yelp" func in utils.py. Default = clean_text_yelp')
-arg_parser.add_argument('--cpfile', default='data_cp8.pk',
+arg_parser.add_argument('--cpfile',
                         help='checkpoint file path which holds training vocabulary, '
                              'embeddings etc. default=data_cp8.pk')
 arg_parser.add_argument('--evaltype', default='forward',
                         help='translate from source to target if value = "forward",'
                              'else reverse.')
 arg_parser.add_argument('--device', default='cuda')
-arg_parser.add_argument('--model', help='file name of saved best model to evaluate')
+arg_parser.add_argument('--model',
+                        help='file name of saved best model to evaluate')
 arg_parser.add_argument('--config',
                         help='configuration/hyperparameters in json format.'
                              ' Default is the config saved with "cpfile"')
@@ -106,26 +110,28 @@ def sent_to_tensor(sentence, word2idx=word2idx, max_len=max_len, type=mode,
 with open(eval_file_path, encoding='utf-8', errors='ignore') as file:
     lines_ = file.readlines()
     lines = [clean_text_func(line) for line in lines_]
-    max_len = max([len(line.split()) for line in lines])+3
+    max_len = max([len(line.split()) for line in lines]) + 3
     tensors = [sent_to_tensor(sent, max_len=max_len) for sent in lines]
     tensors = torch.stack(tensors)
 
 word_emb_src = torch.tensor(word_emb_src)
-word_emb_src = word_emb_src/torch.norm(word_emb_src, dim=1).unsqueeze(-1)
+word_emb_src = word_emb_src / torch.norm(word_emb_src, dim=1).unsqueeze(-1)
 
 word_emb_tgt = torch.tensor(word_emb_tgt)
-word_emb_tgt = word_emb_tgt/torch.norm(word_emb_tgt, dim=1).unsqueeze(-1)
+word_emb_tgt = word_emb_tgt / torch.norm(word_emb_tgt, dim=1).unsqueeze(-1)
 
-generator = GeneratorModel(len(word2idx_src), len(word2idx_tgt), config_dict['hidden_dim'],
-               config_dict['batch_size'], word_emb_src, word_emb_tgt,
-               device, layers_gen=config_dict['layers_gen'],
-               layers_dec=config_dict['layers_dec'],
-               bidir_gen=config_dict['bidir_gen'],
-               bidir_dec=config_dict['bidir_dec'],
-               lstm_do=config_dict['lstm_do'],
-               use_attn=config_dict['use_attention'],
-               emb_do=config_dict['emb_do'])  # no word_dropout needed
-
+generator = GeneratorModel(len(word2idx_src), len(word2idx_tgt),
+                           config_dict['hidden_dim'],
+                           config_dict['embedding_dim'],
+                           config_dict['batch_size'], word_emb_src,
+                           word_emb_tgt, device,
+                           layers_gen=config_dict['layers_gen'],
+                           layers_dec=config_dict['layers_dec'],
+                           bidir_gen=config_dict['bidir_gen'],
+                           bidir_dec=config_dict['bidir_dec'],
+                           lstm_do=config_dict['lstm_do'],
+                           use_attn=config_dict['use_attention'],
+                           emb_do=config_dict['emb_do'])  # no word_dropout needed
 
 # state = torch.load(resume_history, map_location='cpu')
 try:
@@ -135,11 +141,12 @@ except Exception as e:
     best_model_dir = best_model_path.parts[-2]
     data_cp_dir = data_cp_path.parts[-2]
     if best_model_dir != data_cp_dir:
-        print('ERROR : cpfile (the checkpoint file which contains model configurations)'
-              ' belongs to expid "{}" and best model checkpoint belongs to "{}". '
-              'If error was due to weight mismatch, please verify if the model '
-              'weights/configuration is same for both expids.'.
-              format(data_cp_dir, best_model_dir))
+        print(
+            'ERROR : cpfile (the checkpoint file which contains model configurations)'
+            ' belongs to expid "{}" and best model checkpoint belongs to "{}". '
+            'If error was due to weight mismatch, please verify if the model '
+            'weights/configuration is same for both expids.'.
+            format(data_cp_dir, best_model_dir))
     sys.exit(0)
 
 generator.to(device)
@@ -161,10 +168,11 @@ def eval_model_tensor(generator, x, mode, batch_size=100):
     input = x if len(x.size()) > 1 \
         else x.view(1, -1)
     sents = []
-    batches = (len(x)-1)//batch_size + 1
+    batches = (len(x) - 1) // batch_size + 1
     for i in range(batches):
-        gen_out, gen_raw, enc_out = generator(input[i*batch_size: (i+1)*batch_size])
-        sents.extend([tensor_to_sentence(row)for row in gen_out])
+        gen_out, gen_raw, enc_out = generator(
+            input[i * batch_size: (i + 1) * batch_size])
+        sents.extend([tensor_to_sentence(row) for row in gen_out])
     return sents
 
 
@@ -173,7 +181,8 @@ candidate_corpus = []
 ref_corpus = []
 for i, line in enumerate(lines):
     print('\n', line.strip(), '-->', ' '.join([w for w in result[i].split(' ')
-                                               if w not in [SOS_TGT, SOS_SRC, 'EOS', 'PAD']]))
+                                               if w not in [SOS_TGT, SOS_SRC,
+                                                            'EOS', 'PAD']]))
     candidate_corpus.append(word_tokenize(result[i]))
     ref_corpus.append(word_tokenize(line))
     # print('bleu', bleu_score(candidate_corpus, ref_corpus))
